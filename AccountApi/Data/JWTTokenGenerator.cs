2e13f8e2ddd6aa7925942305efb335b722015299
harshvachhani04 <harshvachhani04@outlook.com>
2025-04-20 22:36:05 +0530

    JWT Authentication

---
+++ AccountApi/Data/JWTTokenGenerator.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AccountApi.Data
+{
+    public class JWTTokenGenerator
+    {
+        private readonly IConfiguration _configuration;
+
+        public JWTTokenGenerator(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+        public string GenerateToken(int userId, string role)
+        {
+            var jwtSetting = _configuration.GetSection("JWTSettings");
+            var credentials= new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSetting["Key"])), SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+                new Claim(ClaimTypes.Role, role)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSetting["Issuer"],
+                audience: jwtSetting["Audience"],
+                expires: DateTime.Now.AddHours(Convert.ToDouble(jwtSetting[""])),
+                signingCredentials: credentials,
+                claims: claims
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
--- AccountApi/Program.cs
+++ AccountApi/Program.cs
@@ -1,5 +1,7 @@
 using AccountApi.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,22 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"))
+);
+
+var jwtSetting = builder.Configuration.GetSection("JWTSettings");
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    {
+        //ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSetting["Issuer"],
+        ValidAudience = jwtSetting["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSetting["Key"]))
+    }
 );
 
 var app = builder.Build();
