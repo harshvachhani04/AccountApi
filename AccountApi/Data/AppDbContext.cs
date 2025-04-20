ce5ea3b6961e51b30a3f36570a221374f2092257
harshvachhani04 <harshvachhani04@outlook.com>
2025-04-20 21:55:32 +0530

    Added User Model and Connected with database

---
+++ AccountApi/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AccountApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ AccountApi/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AccountApi.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Compare("Password", ErrorMessage = "Password and Confirm Password not match")]
+        public string ConfirmPassword { get; set; }
+        [AllowedValues("user", "admin")]
+        public string Role { get; set; }
+    }
+}
